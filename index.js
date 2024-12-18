024a04ddf5186d2d59c373a89689f3a854f383f4
Alysson Araujo <alyssonmusic@live.com>
2024-12-18 11:41:42 -0300

    Criando o filtro da contagem de mensagens e adicionando os comentários de cada filtro

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ function filtrarPorAssunto(contatos, assuntoDesejado) {
 }
 const mensagens = filtrarPorAssunto(contatos, "Suporte Técnico"); // <- ALTERAR AQUI O ASSUNTO
 
+// Filtrar mensagens com base no assunto
 console.log("Mensagens com Assunto: ");
 mensagens.forEach((contato, index) => {
   console.log(`${index + 1}. ${contato.mensagem}`);
 });
 
+// Listar os contatos classificados por assunto
+console.log("\nContatos por Assunto:");
+mensagens.forEach((contato, index) => {
+  console.log(`${index + 1}. ${contato.nome} (${contato.email})`);
+})
+
+
+// Exibir uma mensagem de quantas mensagens existem em cada categoria
 console.log(
   `\nTotal de mensagens: ${contatos.length} \n  Dúvida: ${
     filtrarPorAssunto(contatos, "Dúvida").length
@@ -17,3 +26,4 @@ console.log(
     filtrarPorAssunto(contatos, "Suporte Técnico").length
   },\n  Sugestão: ${filtrarPorAssunto(contatos, "Sugestão").length}.`
 );
+
