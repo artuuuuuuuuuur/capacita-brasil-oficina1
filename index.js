a53b2a79406eeea24d3e08ce6ff6689426f762e9
Artur de Maria <arturdemaria7@gmail.com>
2024-12-18 11:24:43 -0300

    add: filtrar mensagens por assunto, exibir total de mensagens

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const contatos = require("./contatos");
- 
 
-
-// filtrar mensagens com base no assunto
-
-const resultado = contatos.filter(filtrar_assunto);
-
-function filtrar_assunto(contato) {
-  return contato.assunto = "sugestão";
+function filtrarPorAssunto(contatos, assuntoDesejado) {
+  return contatos.filter((contato) => contato.assunto === assuntoDesejado);
 }
+const mensagens = filtrarPorAssunto(contatos, "Suporte Técnico"); // <- ALTERAR AQUI O ASSUNTO
 
+console.log("Mensagens com Assunto: ");
+mensagens.forEach((contato, index) => {
+  console.log(`${index + 1}. ${contato.mensagem}`);
+});
 
-console.log(resultado);
-
+console.log(
+  `\nTotal de mensagens: ${contatos.length} \n  Dúvida: ${
+    filtrarPorAssunto(contatos, "Dúvida").length
+  },\n  Suporte Técnico: ${
+    filtrarPorAssunto(contatos, "Suporte Técnico").length
+  },\n  Sugestão: ${filtrarPorAssunto(contatos, "Sugestão").length}.`
+);
